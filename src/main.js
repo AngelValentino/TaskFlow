472b2d571206da9d21dbd78bda4a8d7dfd692279
AngelValentino <138892399+AngelValentino@users.noreply.github.com>
2024-02-26 18:05:36 +0100

    Changed css reset and implemented addTodoPrompt function test.

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
+const addTodoBtnLm = document.getElementById('todo__intro-add-btn');
+const refreshQuoteBtn = document.getElementById('quote__btn');
+
 async function getQuoteData() {
   const response = await  fetch('/.netlify/functions/fetch-data');
 
@@ -8,7 +11,31 @@ async function getQuoteData() {
   return await response.json();
 }
 
-document.getElementById('quote__btn').addEventListener('click', () => {
+function addTodoPrompt() {
+
+  //refactor this into proper code
+  //close icon closes prompt
+  //sbmit gets the info and closes prompt
+  //form validation
+  const addTodoPromptLm = document.getElementById('todo__add-todo-prompt');
+
+  addTodoPromptLm.classList.toggle('todo__add-todo-prompt--active');
+
+  if (addTodoPromptLm.matches('.todo__add-todo-prompt--active')) {
+    addTodoPromptLm.style.maxHeight = '1000px';
+    addTodoBtnLm.style.backgroundColor = "#b259b6";
+    addTodoPromptLm.style.transform = 'translateY(0px)';
+  } 
+  else {
+    addTodoPromptLm.style.maxHeight = 0;
+    addTodoBtnLm.style.backgroundColor = "#593e7f";
+    addTodoPromptLm.style.transform = 'translateY(-150px)';
+  }
+}
+
+addTodoBtnLm.addEventListener('click', addTodoPrompt);
+
+refreshQuoteBtn.addEventListener('click', () => {
   getQuoteData()
     .then(data => console.log(data))
     .catch(err => console.err(err));
