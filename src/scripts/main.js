bfb411d4ea6a7334e47f8801c8adf1ee30816adf
AngelValentino <138892399+AngelValentino@users.noreply.github.com>
2024-03-08 14:36:03 +0100

    Added alert modal dialog.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,5 @@
+import {openModal, addEventsToAlertDialogBtns} from './alertDialog.js';
+
 const refreshQuoteBtn = document.getElementById('quote__btn');
 const addTodoPromptFormLm = document.getElementById('todo-app-prompt__form');
 const addTodoPromptCloseBtn = document.getElementById('todo-app-prompt__cancel-btn');
@@ -36,23 +38,26 @@ const introPrompts = {
   //Connect showAddPrompt and showSearchPrompt animations so only one prompt can be shown at once. 
 */
 
+/* Completed form validation and todo data object.
   // Form validation; There must be at least a title to send the form.
   // If information is added to the add todo prompt and is closed, a confirmation modal will appear to make sure if the user wants to discard the changes. This also has to work interchangeably with the search prompt, as the animations will be interlinked.
   
   // Add a form submit to get the todo data and send it to and array of objects. The maximum incompleted tasks are 100.
-  /*
-  {
-    id: 'task-[1-100]',
-    title: '',
-    date: ''/null,
-    description: ''/null,
-    completed: true/false
-  }
-  */
+  
+  // {
+  //   id: 'task-[1-100]',
+  //   title: '',
+  //   date: ''/null,
+  //   description: ''/null,
+  //   completed: true/false
+  // } 
+*/
 
-  // Render all tasks HTML
+  // generateHTML
+  
+  // Show dialog if the user wants to add a todo and there are more than 100 incompleted tasks. Max tasks limit reached, close message and reset form.
 
-  // Implement remove task and render HTML seamlessly
+  // Implement remove task and generateHTML seamlessly
 
 
 // Todo next week
@@ -158,6 +163,15 @@ function showSearchTodoPrompt(e) {
   togglePrompt(searchTodoPrompt, addTodoPrompt, e);
 }
 
+function resetForm() {
+  const {promptLm, btnLm, activeClass, timeout: {time}} = introPrompts.addTodoPrompt;
+  addTodoPromptFormLm.reset();
+  checkActiveBtn(btnLm);
+  hidePrompt(promptLm, btnLm, activeClass, 'alertDialogDiscardChangesTim', time);
+}
+
+addEventsToAlertDialogBtns(resetForm);
+
 introPrompts.addTodoPrompt.btnLm.addEventListener('click', showAddTodoPrompt);
 
 introPrompts.searchTodoPrompt.btnLm.addEventListener('click', showSearchTodoPrompt);
@@ -181,12 +195,12 @@ addTodoPromptFormLm.addEventListener('submit', (e) => {
 } );
 
 addTodoPromptCloseBtn.addEventListener('click', () => {
+  const {btnLm, promptLm, activeClass, timeout: {time}} = introPrompts.addTodoPrompt;
   const todoData = Object.values(getFormData(addTodoPromptFormLm));
   if(todoData[0] || todoData[1] || todoData[2]) {
     openModal();
   } 
   else {
-    const {btnLm, promptLm, activeClass, timeout: {time}} = introPrompts.addTodoPrompt;
     checkActiveBtn(btnLm);
     hidePrompt(promptLm, btnLm, activeClass, 'closeAddTodoPromptTim', time);
   }
@@ -197,90 +211,3 @@ refreshQuoteBtn.addEventListener('click', () => {
     .then(data => console.log(data))
     .catch(err => console.err(err));
 }); 
-
-
-// Refactor into more simpler and reusable code, maybe add event listener with openModal() and remove events with closeModal().
-
-const dialogBackdropLm = document.getElementById('dialog-backdrop');
-const closeDialogBtn = document.getElementById('alert-dialog__cancel-btn');
-const confirmationBtn = document.getElementById('alert-dialog__confirmation-btn');
-const discardBtn = document.getElementById('alert-dialog__discard-btn');
-const alertDialogLm = document.getElementById('alert-dialog');
-let lastFocusLmBeforeAlertDialog;
-
-function openModal() {
-  lastFocusLmBeforeAlertDialog = document.activeElement;
-  dialogBackdropLm.style.display = 'flex';
-  discardBtn.focus();
-  setTimeout(() => {
-    dialogBackdropLm.classList.add('alert-dialog-backdrop--active');
-    alertDialogLm.classList.add('alert-dialog--active');
-  });
-}
-
-function closeModal() {
-  dialogBackdropLm.classList.remove('alert-dialog-backdrop--active')
-  alertDialogLm.classList.remove('alert-dialog--active');
-  setTimeout(() => {
-    dialogBackdropLm.style.display = 'none';
-  }, 250);
-  lastFocusLmBeforeAlertDialog.focus();
-}
-
-function trapFocus(element) {
-  const focusableLms = element.querySelectorAll('a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input[type="text"]:not([disabled]), input[type="radio"]:not([disabled]), input[type="checkbox"]:not([disabled]), select:not([disabled])');
-  console.log(focusableLms);
-  const firstFocusableLm = focusableLms[0]; 
-  const lastFocusableLm = focusableLms[focusableLms.length - 1];
-
-  element.addEventListener('keydown', (e) => {
-    const isTabPressed = (e.key === 'Tab');
-    
-    if (!isTabPressed) { 
-      return; 
-    }
-
-    if (e.shiftKey) /* shift + tab */ {
-      if (document.activeElement === firstFocusableLm ) {
-        lastFocusableLm.focus();
-        e.preventDefault();
-      }
-    } 
-    else /* tab */ {
-      if (document.activeElement === lastFocusableLm) {
-        firstFocusableLm.focus();
-        e.preventDefault();
-      }
-    }
-  });
-}
-
-trapFocus(alertDialogLm);
-
-dialogBackdropLm.addEventListener('click', (e) => {
-  if (!e.target.matches('.dialog-backdrop')) {
-    return;
-  } 
-  closeModal();
-});
-
-closeDialogBtn.addEventListener('click', closeModal);
-
-confirmationBtn.addEventListener('click', () => {
-  closeModal();
-  addTodoPromptFormLm.reset();
-  const {promptLm, btnLm, activeClass, timeout: {time}} = introPrompts.addTodoPrompt;
-  checkActiveBtn(btnLm);
-
-  // if searchBtn is clicked and alertDialogDiscardChangesTim skip clear interval
-  hidePrompt(promptLm, btnLm, activeClass, 'alertDialogDiscardChangesTim', time)
-
-});
-
-discardBtn.addEventListener('click', closeModal);
-
-discardBtn.addEventListener('keydown', (e) => {
-  if (e.key === 'Escape') {
-    closeModal();
-  }
-});
