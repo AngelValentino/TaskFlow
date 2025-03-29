7cdb9e6808251df8054ae1d78a39072e906d007e
AngelValentino <138892399+AngelValentino@users.noreply.github.com>
2025-03-29 13:29:07 +0100

    Styled main padding for all views to accommodate the user menu.

--- frontend/src/pages/Logout.js
+++ frontend/src/pages/Logout.js
@@ -1,7 +1,7 @@
 export default class LogoutPage {
   static getHtml() {
     return `
-      <main>
+      <main class="logout-view">
         <p id="logout-message" class="logout-message">Logging out, please wait...</p>
         <p class="return-home-message">You may <a class="return-home-link" href="/" data-link>return home</a>.</p>
       </main>
